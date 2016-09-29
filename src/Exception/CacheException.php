51da833ede63548eb4a7c40be51b381b0fa55949
k.savchev@gmail.com <k.savchev@viscomp.bg>
2016-09-29 11:45:51 +0300

    Add exception classes

---
+++ src/Exception/CacheException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Ite\Cache\Exception;
+
+use Ite\Cache\Exception\CacheException as CacheExceptionInterface;
+
+/**
+ * CacheException
+ *
+ * @author Kiril Savchev <k.savchev@gmail.com>
+ */
+class CacheException extends \Exception implements CacheExceptionInterface {
+
+}
