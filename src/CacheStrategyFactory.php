0172df9abec06e452af0dddbf7dde06c716beb61
k.savchev@gmail.com <k.savchev@viscomp.bg>
2016-09-30 17:28:19 +0300

    Finish the strategy factory

--- src/CacheStrategyFactory.php
+++ src/CacheStrategyFactory.php
@@ -9,12 +9,26 @@ namespace Ite\Cache;
  */
 class CacheStrategyFactory {
 
+        /**
+         * Seconds in one hour
+         */
         const HOUR = 3600;
 
+        /**
+         * Seconds in one day
+         */
         const DAY = 86400;
 
+        /**
+         * Expire key for parameters
+         */
         const EXPIRE_KEY = 'expire';
 
+        /**
+         * Added strategy' params
+         *
+         * @var array
+         */
         protected static $cacheStrategies = [
                 'file' => [
                         'class' => FileCache::class,
@@ -33,6 +47,92 @@ class CacheStrategyFactory {
 
         ];
 
+        /**
+         * registered strategy to internal container
+         *
+         * @param string $name
+         * @param array|string $params
+         * @throws Exception\InvalidArgumentException
+         */
+        public static function addStrategy($name, $params) {
+                if (array_key_exists($name, static::$cacheStrategies)) {
+                        throw new Exception\InvalidArgumentException("Strategy {$name} is already registered");
+                }
+                if (is_string($params)) {
+                        static::$cacheStrategies[$name] = [ 'class' => $params];
+                }
+                else if (is_array($params)) {
+                        if (!array_key_exists('class', $params)) {
+                                throw new Exception\InvalidArgumentException("No strategy class defined");
+                        }
+                        static::$cacheStrategies[$name] = $params;
+                }
+                else {
+                        throw new Exception\InvalidArgumentException("Invalid parameter name");
+                }
+        }
+
+        /**
+         * Checks whether a strategy is registered
+         * @param string $name
+         * @return bool
+         */
+        public static function hasStrategy($name) {
+                return array_key_exists($name, static::$cacheStrategies);
+        }
+
+        /**
+         * Unregister strategy
+         *
+         * @param string $name
+         */
+        public static function removeStrategy($name) {
+                if (static::hasStrategy($name)) {
+                        unset(static::$cacheStrategies[$name]);
+                }
+        }
+
+        /**
+         * Dynamic alias of self::addStrategy()
+         *
+         * @param string $name
+         * @param array|string $params
+         */
+        public function addCacheStrategy($name, $params) {
+                static::addStrategy($name, $params);
+        }
+
+        /**
+         * Dynamic alias of self::hasStrategy()
+         *
+         * @param string $name
+         * @return bool
+         */
+        public function hasCacheStrategy($name) {
+                return static::hasStrategy($name);
+        }
+
+        /**
+         * Dynamic alias of self::removeStrategy()
+         *
+         * @param string $name
+         */
+        public function removeCacheStrategy($name) {
+                static::removeStrategy($name);
+        }
+
+        /**
+         * Creates a cache pool object
+         *
+         * It merges the default registered parameters with the second parameter
+         *
+         * @param string $name The registered cache strategy
+         * @param array $params [Optional] Parameters for the pool
+         * @return \Psr\Cache\CachePoolInterface
+         *
+         * @throws Exception\InvalidArgumentException If the wanted strategy is not registered or is with invalid parameters
+         * @throws Exception\CacheException If the pool class is not a valid PSR-6 item pool class
+         */
         public function create($name, array $params = []) {
                 if (!array_key_exists($name, static::$cacheStrategies)) {
                         throw new Exception\InvalidArgumentException("Unknown cache strategy {$name}");
